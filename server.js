4fa88d02375956a37c2bd92503afad7ce8c4d314
Sense <58160665@go.buu.ac.th>
2020-07-04 16:37:10 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+//ทำการ import express เข้ามาใช้งาน โดยสร้างตัวแปร express ขึ้นมาเพื่อรับค่า
+const express = require('express')
+const db = require('./db_connection')
+const indexController = require('./app/controller/Indexcontroller')
+var bodyParser = require('body-parser')
+//ทำการสร้าง Instance ของ express และสร้างตัวแปร app ขึ้นมาเพื่อรับค่า
+const app = express()
+
+db.connection()
+//สร้างตัวแปร PORT ขึ้นมารับค่า port ในกรณีที่เราได้กำหนดไว้ใน environment ของเครื่อง
+//แต่ถ้าไม่ได้กำหนดไว้ เราจะใช้ค่า 8080 แทน
+const PORT = process.env.PORT || 8080
+//สร้าง route ขึ้นมา 1 ตัว โดยกำหนดให้ path คือ / หรือ index ของ host นั่นเอง
+//จากนั้นให้กำหนด response แสดงคำว่า Hello World
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json)
+app.get('/', (req, res) => res.send('Hello World'))
+
+app.post('/create', (req,res) => { 
+    console.log("123"); 
+    console.log(req.body.number)
+    indexController.insert(req , res) 
+})
+//run web server ที่เราสร้างไว้ โดยใช้ PORT ที่เรากำหนดไว้ในตัวแปร PORT
+app.listen(PORT, () => {
+    //หากทำการ run server สำเร็จ ให้แสดงข้อความนี้ใน cmd หรือ terminal
+    console.log(`Server is running on port : ${PORT}`)
+})
+//ทำการ export app ที่เราสร้างขึ้น เพื่อให้สามารถนำไปใช้งานใน project อื่นๆ 
+//เปรียบเสมือนเป็น module ตัวนึง
+module.exports = app
